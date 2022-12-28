b8a88299e282df8645d1d56bf59ef915e616f8a2
mnokno <prv.nowacki@gmail.com>
2022-12-28 17:54:02 +0000

    Update GameManager.cs

--- Connect4/Assets/Scripts/GameManager.cs
+++ Connect4/Assets/Scripts/GameManager.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    /// <summary>
+    /// Stores reference's to gameCircles
+    /// </summary>
+    private GameCircle[,] gameCircles;
+    /// <summary>
+    /// Stores reference's to gameBoard (logical structures following game rules)
+    /// </summary>
+    private GameBoard gameBoard;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameCircles = new GameCircle[7, 6];
+        gameBoard = new GameBoard();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    /// <summary>
+    /// Adds a gameCircle to the gameCircles array
+    /// </summary>
+    /// <param name="gameCircle">gameCircle to register</param>
+    public void RegisterGameCircle(GameCircle gameCircle)
+    {
+        gameCircles[gameCircle.GetX(), gameCircle.GetY()] = gameCircle;
+    }
+
+    /// <summary>
+    /// Handles user input
+    /// </summary>
+    /// <param name="gameCircle">Clicked circle</param>
+    public void HandleClicked(GameCircle gameCircle)
+    {
+        int x = gameCircle.GetX();
+        int y = gameCircle.GetY();
     }
 }
