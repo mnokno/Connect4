ec887673715540d9a5007c89d4967f7049cae4b3
mnokno <prv.nowacki@gmail.com>
2022-12-28 17:42:07 +0000

    Finished GameBoard

--- Connect4/Assets/Scripts/GameBoard.cs
+++ Connect4/Assets/Scripts/GameBoard.cs
@@ -1,18 +1,307 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class GameBoard : MonoBehaviour
+public class GameBoard
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// The board is a 2D array of tiles
+    /// </summary>
+    private TileState[,] gameBoard = new TileState[7,6];
+    /// <summary>
+    /// Keeps track of the next player to move
+    /// </summary>
+    private Player nextPlayerToMove;
+    /// <summary>
+    /// Keeps track of the game state
+    /// </summary>
+    private GameState gameState;
+    /// <summary>
+    /// Keeps track of the number of moves made, if 42 and the game is still ON_GOING, it's a draw
+    /// </summary>
+    private int moveCount;
+
+    /// <summary>
+    /// Constructor for GameBoard, returns a new GameBoard with all trackers set to the new empty game
+    /// </summary>
+    public GameBoard()
     {
-        
+        // Initialize the board to empty
+        for (int x = 0; x < 7; x++)
+        {
+            for (int y = 0; y < 6; y++)
+            {
+                gameBoard[x, y] = TileState.EMPTY;
+            }
+        }
+        // Yellow always start the game
+        nextPlayerToMove = Player.YELLOW;
+        // Initially the game is on going
+        gameState = GameState.ON_GOING;
+        // No moves have been made
+        moveCount = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Make a move on the board (only if the move is legal) updating all trackers
+    /// </summary>
+    /// 
+    /// <param name="x">X position of where we want to make the move</param>
+    /// <param name="y">Y position of where we want to make the move</param>
+    /// <returns>True if the move was played, false otherwise</returns>
+    public bool MakeMove(int x, int y)
     {
-        
+        if (IsMoveLegal(x, y))
+        {
+            // Plays the move
+            gameBoard[x, y] = (TileState)(int)nextPlayerToMove;
+            // Updates next play to move
+            nextPlayerToMove = nextPlayerToMove == Player.YELLOW ? Player.RED : Player.YELLOW;
+            // Updates move count
+            moveCount++;
+            // Updates game state
+            UpdateGameState();
+            // The move was successfully played
+            return true;
+        }
+        else
+        {
+            // The move could not be played become its not legal
+            return false;
+        }
     }
+
+    /// <summary>
+    /// Checks if the given move is legal
+    /// </summary>
+    /// 
+    /// <param name="x">X position of the move</param>
+    /// <param name="y">Y position of the move</param>
+    /// <returns>True if the move is legal, false otherwise</returns>
+    public bool IsMoveLegal(int x, int y)
+    {
+        if (gameBoard[x,y] != TileState.EMPTY)
+        {   
+            // if this tile is not empty than this move cant be legal
+            return false;
+        }
+        else
+        {
+            // if the tile below this one is not the floor or empty than this move can't be legal, otherwise it has to be legal
+            if (y == 0 || gameBoard[x, y -1] != TileState.EMPTY)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Updates game state brute-forcing all wining possibilities, this is not very efficient but this class
+    /// will only by used to mange and keep track of the game for UI peruses not heavy calculation
+    /// </summary>
+    private void UpdateGameState()
+    {
+        // Checks vertical lines
+        for (int x = 0; x < 7; x++)
+        {
+            TileState lineType = TileState.EMPTY;
+            int count = 0;
+            for (int y = 0; y < 6; y++)
+            {
+                if (gameBoard[x, y] == lineType)
+                {
+                    count++;
+                }
+                else
+                {
+                    lineType = gameBoard[x, y];
+                    count = 1;
+                }
+
+                if (count == 4 || lineType != TileState.EMPTY)
+                {
+                    gameState = (GameState)(int)lineType;
+                    return;
+                }
+            }
+        }
+        // Checks horizontal lines
+        for (int y = 0; y < 6; y++)
+        {
+            TileState lineType = TileState.EMPTY;
+            int count = 0;
+            for (int x = 0; x < 7; x++)
+            {
+                if (gameBoard[x, y] == lineType)
+                {
+                    count++;
+                }
+                else
+                {
+                    lineType = gameBoard[x, y];
+                    count = 1;
+                }
+
+                if (count == 4 || lineType != TileState.EMPTY)
+                {
+                    gameState = (GameState)(int)lineType;
+                    return;
+                }
+            }
+        }
+        // Checks diagonal line with positive gradient
+        for (int x = 0; x < 7; x++)
+        {
+            TileState lineType = TileState.EMPTY;
+            int count = 0;
+            int currentX = x;
+            int currentY = 0;          
+            while (currentX >= 7 && currentY >= 6)
+            {
+                if (gameBoard[currentX, currentY] == lineType)
+                {
+                    count++;
+                }
+                else
+                {
+                    lineType = gameBoard[currentX, currentY];
+                    count = 1;
+                }
+                if (count == 4 || lineType != TileState.EMPTY)
+                {
+                    gameState = (GameState)(int)lineType;
+                    return;
+                }
+                currentX++;
+                currentY++;
+            }
+        }
+        for (int y = 0; y < 6; y++)
+        {
+            TileState lineType = TileState.EMPTY;
+            int count = 0;
+            int currentX = 0;
+            int currentY = y;
+            while (currentX >= 7 && currentY >= 6)
+            {
+                if (gameBoard[currentX, currentY] == lineType)
+                {
+                    count++;
+                }
+                else
+                {
+                    lineType = gameBoard[currentX, currentY];
+                    count = 1;
+                }
+                if (count == 4 || lineType != TileState.EMPTY)
+                {
+                    gameState = (GameState)(int)lineType;
+                    return;
+                }
+                currentX++;
+                currentY++;
+            }
+        }
+        // Checks diagonal line with negative gradient
+        for (int x = 0; x < 7; x++)
+        {
+            TileState lineType = TileState.EMPTY;
+            int count = 0;
+            int currentX = x;
+            int currentY = 6;
+            while (currentX >= 7 && currentY >= 6)
+            {
+                if (gameBoard[currentX, currentY] == lineType)
+                {
+                    count++;
+                }
+                else
+                {
+                    lineType = gameBoard[currentX, currentY];
+                    count = 1;
+                }
+                if (count == 4 || lineType != TileState.EMPTY)
+                {
+                    gameState = (GameState)(int)lineType;
+                    return;
+                }
+                currentX++;
+                currentY--;
+            }
+        }
+        for (int y = 0; y < 6; y++)
+        {
+            TileState lineType = TileState.EMPTY;
+            int count = 0;
+            int currentX = 0;
+            int currentY = y;
+            while (currentX >= 7 && currentY >= 6)
+            {
+                if (gameBoard[currentX, currentY] == lineType)
+                {
+                    count++;
+                }
+                else
+                {
+                    lineType = gameBoard[currentX, currentY];
+                    count = 1;
+                }
+                if (count == 4 || lineType != TileState.EMPTY)
+                {
+                    gameState = (GameState)(int)lineType;
+                    return;
+                }
+                currentX++;
+                currentY--;
+            }
+        }
+        // Check if its a draw
+        if (moveCount == 42)
+        {
+            gameState = GameState.DRAW;
+            return;
+        }
+    }
+
+    #region Getters and Setters
+
+    /// <summary>
+    /// Getter for the game board
+    /// </summary>
+    /// <returns>gameBoard</returns>
+    public TileState[,] GetGameBoard()
+    {
+        return gameBoard;
+    }
+
+    /// <summary>
+    /// Getter for the next player to move
+    /// </summary>
+    /// <returns>nextPlayerToMove</returns>
+    public Player GetNextPlayerToMove()
+    {
+        return nextPlayerToMove;
+    }
+
+    /// <summary>
+    /// Getter for the game state
+    /// </summary>
+    /// <returns>gameState</returns>
+    public GameState GetGameState()
+    {
+        return gameState;
+    }
+
+    /// <summary>
+    /// Getter for the move count
+    /// </summary>
+    /// <returns>moveCount</returns>
+    public int GetMoveCount()
+    {
+        return moveCount;
+    }
+
+    #endregion
+
 }
