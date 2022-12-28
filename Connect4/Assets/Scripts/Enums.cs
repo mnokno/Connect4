f606ee9c8d52603c6e8ed08b83b082cba35f613d
mnokno <prv.nowacki@gmail.com>
2022-12-28 17:04:58 +0000

    Crated file to hold enums

---
+++ Connect4/Assets/Scripts/Enums.cs
@@ -0,0 +1,20 @@
+enum TileState
+{
+    YELLOW = 0,
+    RED = 1,
+    EMPTY = 2
+}
+
+enum Player
+{
+    YELLOW = 0,
+    RED = 1
+}
+
+enum GameState
+{
+    YELLOW_WON = 0,
+    RED_WON = 1,
+    DRAW = 2,
+    ON_GOING = 3
+}
