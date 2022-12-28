849633b47f868f510df3ab6edd5543ebfd9e5ef9
mnokno <prv.nowacki@gmail.com>
2022-12-28 17:51:33 +0000

    Update GameCircle.cs

--- Connect4/Assets/Scripts/GameCircle.cs
+++ Connect4/Assets/Scripts/GameCircle.cs
@@ -5,10 +5,25 @@ using UnityEngine.UI;
 
 public class GameCircle : MonoBehaviour
 {
+    /// <summary>
+    /// Reference to yellow color to use
+    /// </summary>
     [SerializeField] Color yellowColor;
+    /// <summary>
+    /// Reference to red color to use
+    /// </summary>
     [SerializeField] Color redColor;
+    /// <summary>
+    /// Reference to the image of this circle, used to update appearance
+    /// </summary>
     private Image image;
+    /// <summary>
+    /// X position of this circle on the game board
+    /// </summary>
     private int x;
+    /// <summary>
+    /// Y position of this circle on the game board
+    /// </summary>
     private int y;
 
     // Start is called before the first frame update
@@ -18,14 +33,44 @@ public class GameCircle : MonoBehaviour
         image.alphaHitTestMinimumThreshold = 0.5f;
     }
 
+    /// <summary>
+    /// On clicked event handler
+    /// </summary>
     public void Clicked()
     {
         image.color = Random.value > 0.5f ? yellowColor : redColor;
     }
 
+    #region Getters and Setters
+    
+    /// <summary>
+    /// Setter for x and y
+    /// </summary>
+    /// <param name="x">new x vale</param>
+    /// <param name="y">new y bale</param>
     public void SetGamePos(int x, int y)
     {
         this.x = x;
         this.y = y;
     }
+
+    /// <summary>
+    /// Getter for x
+    /// </summary>
+    /// <returns>x</returns>
+    public int GetX()
+    {
+        return x;
+    }
+
+    /// <summary>
+    /// Getter for y
+    /// </summary>
+    /// <returns>y</returns>
+    public int GetY()
+    {
+        return y;
+    }
+
+    #endregion
 }
