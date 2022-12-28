910c14e664583e5d3863ccdfdaebc8b017c802f9
mnokno <prv.nowacki@gmail.com>
2022-12-28 17:21:05 +0000

    Updated enums

--- Connect4/Assets/Scripts/Enums.cs
+++ Connect4/Assets/Scripts/Enums.cs
@@ -1,17 +1,17 @@
-enum TileState
+public enum TileState
 {
     YELLOW = 0,
     RED = 1,
     EMPTY = 2
 }
 
-enum Player
+public enum Player
 {
     YELLOW = 0,
     RED = 1
 }
 
-enum GameState
+public enum GameState
 {
     YELLOW_WON = 0,
     RED_WON = 1,
