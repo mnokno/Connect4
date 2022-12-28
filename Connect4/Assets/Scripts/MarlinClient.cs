88b947fb6cdc7c5ec9f733eb88341e2e208f2946
mnokno <prv.nowacki@gmail.com>
2022-12-28 21:08:51 +0000

    Update MarlinClient.cs

--- Connect4/Assets/Scripts/MarlinClient.cs
+++ Connect4/Assets/Scripts/MarlinClient.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 using System;
 using System.Net;
 using System.Net.Sockets;
@@ -5,12 +7,28 @@ using System.Text;
 
 public class MarlinClient
 {
+    Socket sender;
+
     /// <summary>
     /// Establishes connection to Marlin server
     /// </summary>
     public void Connect()
     {
-        
+        // Establish the remote endpoint for the socket.
+        // Uses port 27015 on the local computer.
+        IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+        IPAddress ipAddr = ipHost.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 27015);
+
+        // Creation TCP/IP Socket using
+        // Socket Class Constructor
+        sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        // Connect Socket to the remote endpoint using method Connect()
+        sender.Connect(localEndPoint);
+
+        // We print EndPoint information that we are connected
+        Debug.Log($"Socket connected to -> {sender.RemoteEndPoint.ToString()} ");
     }
 
     /// <summary>
@@ -18,7 +36,9 @@ public class MarlinClient
     /// </summary>
     public void Disconnect()
     {
-        
+        // Close Socket using the method Close()
+        sender.Shutdown(SocketShutdown.Both);
+        sender.Close();
     }
 
     /// <summary>
@@ -27,7 +47,17 @@ public class MarlinClient
     /// <param name="TTMemoryPool">How much ram in MB the server will dedicate for the game</param>
     public void InitGame(int TTMemoryPool)
     {
-        
+        // Creation of message that we will send to Server
+        byte[] messageSent = Encoding.ASCII.GetBytes("requestType:initialization,TTMemoryPool:" + TTMemoryPool.ToString());
+        int byteSent = sender.Send(messageSent);
+
+        // Data buffer
+        byte[] messageReceived = new byte[1024];
+
+        // We receive the message using the method Receive().
+        // This method returns number of bytes received, that we'll use to convert them to string
+        int byteRecv = sender.Receive(messageReceived);
+        Debug.Log($"Message from Server -> {Encoding.ASCII.GetString(messageReceived, 0, byteRecv)}");
     }
 
     /// <summary>
@@ -38,6 +68,17 @@ public class MarlinClient
     /// <returns></returns>
     public int GetMove(int playedFile, int miliseconds)
     {
+        // Creation of message that we will send to Server
+        byte[] messageSent = Encoding.ASCII.GetBytes("requestType:moveCalculation,opponentMove:" + playedFile.ToString() + ",timeLimit:" + miliseconds.ToString());
+        int byteSent = sender.Send(messageSent);
+
+        // Data buffer
+        byte[] messageReceived = new byte[1024];
+
+        // We receive the message using the method Receive().
+        // This method returns number of bytes received, that we'll use to convert them to string
+        int byteRecv = sender.Receive(messageReceived);
+        Debug.Log($"Message from Server -> {Encoding.ASCII.GetString(messageReceived, 0, byteRecv)}");
         return 0;    
     }
 }
