c1a22f17e83a612f167bd59a29722f705a789631
mnokno <prv.nowacki@gmail.com>
2023-01-02 19:12:59 +0000

    Update NetworkGate.cs

--- Connect4/Assets/Scripts/NetworkGate.cs
+++ Connect4/Assets/Scripts/NetworkGate.cs
@@ -1,7 +1,42 @@
-using UnityEngine;
 using Unity.Netcode;
 
 public class NetworkGate : NetworkBehaviour
 {
-    
+    /// <summary>
+    /// Stores reference to MarlinClient instance, can only be crated on windows machine
+    /// </summary>
+    private MarlinClient marlinClient;
+
+    /// <summary>
+    /// On network spawn initializes marlin client if its the sever side
+    /// </summary>
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            marlinClient = new MarlinClient();
+            marlinClient.InitGame(10000);
+        }
+    }
+
+    /// <summary>
+    /// Calculates engine move on the server side and sends it to the client through MakeAIMoveClientRpc
+    /// Call with user move:-1 to tell the server to calculate the first move
+    /// </summary>
+    /// <param name="userMove">Move made by the user on the client side</param>
+    [ServerRpc]
+    public void RequestAIMoveServerRpc(int userMove)
+    {
+
+    }
+
+    /// <summary>
+    /// Makes the move on the client side, used to send data back to the client
+    /// </summary>
+    /// <param name="aiMove">Move made by the engine</param>
+    [ClientRpc]
+    public void MakeAIMoveClientRpc(int aiMove)
+    {
+
+    }
 }
