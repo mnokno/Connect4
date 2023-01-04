ad28740911dcded1f942734dc5d8629db9c46788
mnokno <prv.nowacki@gmail.com>
2023-01-04 21:37:15 +0000

    Organized UIDocuments in the scene

---
+++ Connect4/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject gameUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
